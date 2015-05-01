48840531e0deecc2feca817c260b39a7e371f50f
Garrett Orme <gorme@1005d-8.eecs.ku.edu>
2015-05-01 09:30:40 -0500

    General House keeping

---
+++ VATM/src/FileWriter.java
@@ -0,0 +1,44 @@
+import java.awt.geom.Arc2D.Double;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class FileWriter {
+	
+	String fileName = null;
+	
+	public boolean FileWrite(String actNum, double balance){
+		
+		
+		try{
+			PrintWriter writer = new PrintWriter(fileName , "UTF-8");
+			writer.write(actNum);
+			writer.write((int) balance);
+			writer.close();
+		}
+		
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean LogWriter(String actNum, double balance){
+		
+		String LogText = null;
+		
+		try{
+			PrintWriter writer = new PrintWriter(fileName , "UTF-8");
+			writer.println(LogText); // logText will be created later.
+			writer.close();
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+}
+
+
